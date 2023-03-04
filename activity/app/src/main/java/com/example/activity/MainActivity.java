e1ef0f0e0c9523180d497253c21e552f95f81d56
suchann <34206192+suchanoname@users.noreply.github.com>
2023-03-04 10:31:10 +0300

    Added activity

---
+++ activity/app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+}
